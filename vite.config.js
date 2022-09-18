138f77ef7052236c8b8fdf4b5eb83993797a860f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-18 11:38:00 +0500

    optimizations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,64 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import { imagetools } from "vite-imagetools";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      threshold: 500,
+    }),
+    ViteWebfontDownload(),
+    chunkSplitPlugin(),
+    imagetools(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.7, 0.8],
+        speed: 4,
+      },
+      webp: {
+        quality: 70,
+      },
+      svgo: {
+        multipass: true,
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "minifyStyles",
+          },
+          {
+            name: "removeMetadata",
+          },
+          {
+            name: "removeUselessStrokeAndFill",
+          },
+          {
+            name: "reusePaths",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: true,
+          },
+        ],
+      },
+    }),
+  ],
 });
